f7928c915dcc42379f802b6a07c0d9dd2b14ce28
Mohammad Taj <peakimo@gmail.com>
2024-09-05 18:31:35 +0330

    test

---
+++ src/main/java/org/peakimo/charity/Charity.java
@@ -0,0 +1,17 @@
+package org.peakimo.charity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Charity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+   // Test
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
